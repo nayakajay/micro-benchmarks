7adec3e77d7e6045dc9c563dc6f7bce4956f3333
Shweta Pandey <shweta_pandey@gitlab.iisc.ac.in>
2019-07-10 18:25:23 +0530

    Vector Add Commit

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 # Straight commands to create runnables
 CUDA_CC=nvcc
 CUDA_FLAGS=-Xptxas -dlcm=cs -Xptxas -dscm=cs -arch=sm_61 -lineinfo
+VECTOR_CUDA_FLAGS=-Xptxas -O0 -Xptxas -dlcm=cg -Xptxas -dscm=wt -arch=sm_61
 
-all: device_alloc host_alloc
+all: device_alloc host_alloc vector_add
 # Executable that creates an array and traverses it, Array allocated in device
 device_alloc:
 	$(CUDA_CC) $(CUDA_FLAGS) src/fine_grain_tlb.cu -o cs_original
@@ -10,3 +11,7 @@ device_alloc:
 # Similar experiment, but memory pinned in host
 host_alloc:
 	$(CUDA_CC) $(CUDA_FLAGS) src/host_alloc.cu -o host_pinned
+
+# Shweta vector addition
+vector_add:
+	$(CUDA_CC) $(VECTOR_CUDA_FLAGS) src/vector_add.cu -o vector_add
--- src/vector_add.cu
+++ src/vector_add.cu
@@ -8,22 +8,15 @@ void add(int n, float *x, float *y, float *z)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   if (i < n) z[i] = x[i] + y[i];
-  //if (i< n) z[i]++;
+  if (i< n) z[i]++;
 }
 
 int main() {
-    int N = 1<<15;
+    int N = 1<<20;
     float *x, *y, *z, *d_x, *d_y, *d_z;
-/*
-    x = (float*)malloc(N*sizeof(float));
-    y = (float*)malloc(N*sizeof(float));
-    z = (float*)malloc(N*sizeof(float));
-*/
-/*
-    cudaMalloc(&d_x, N*sizeof(float));
-    cudaMalloc(&d_y, N*sizeof(float));
-    cudaMalloc(&d_z, N*sizeof(float));
-*/
+
+    cudaDeviceReset();
+    //Allocating memory onto host
     cudaHostAlloc((void **)&x,  N*sizeof(float), cudaHostAllocMapped );
     cudaHostAlloc((void **)&y,  N*sizeof(float), cudaHostAllocMapped );
     cudaHostAlloc((void **)&z,  N*sizeof(float), cudaHostAllocMapped );
@@ -31,17 +24,12 @@ int main() {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
-/*
-    cudaMemcpy(d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_z, z, N*sizeof(float), cudaMemcpyHostToDevice);
-*/
 
+    //Getting device pointer
     cudaHostGetDevicePointer((void **)&d_x, x, 0);
     cudaHostGetDevicePointer((void **)&d_y, y, 0);
     cudaHostGetDevicePointer((void **)&d_z, z, 0);
 
-    //cudaDeviceReset();
     cudaDeviceSynchronize();
     cudaProfilerStart();
     add<<<(N+255)/256, 256>>>(N, d_x, d_y, d_z);
@@ -54,10 +42,8 @@ int main() {
     }
 
     printf("Sum = %f\n", sum);
-    cudaFree(d_x);
-    cudaFree(d_y);
-    cudaFree(d_z);
     cudaFreeHost(x);
     cudaFreeHost(y);
     cudaFreeHost(z);
+    cudaDeviceReset();
 }
